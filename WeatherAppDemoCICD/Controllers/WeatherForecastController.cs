e31b553b841d188c065f4072f7b9884b2b70d5a3
gitalvininfo <yanson.alvin.info@gmail.com>
2024-10-21 11:55:39 +0800

    feat: test connection fix

--- WeatherAppDemoCICD/Controllers/WeatherForecastController.cs
+++ WeatherAppDemoCICD/Controllers/WeatherForecastController.cs
@@ -18,19 +18,20 @@ namespace WeatherAppDemoCICD.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("WeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
+                TemperatureC = Random.Shared.Next(-
+                20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
 
-        [HttpGet]
+        [HttpGet("Test")]
         public IActionResult Test()
         {
             return Ok("Test connection worked");
